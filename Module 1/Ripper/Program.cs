ee3baae19bebd8243ead743dd63b81bdd0d870fd
Yuri1965 <ura747@mail.ru>
2017-02-20 22:53:07 +0600

    first commit

---
+++ Module 1/Ripper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using CsQuery;
+
+using RipperManagerLib;
+
+namespace Ripper
+{
+    class Program
+    {
+        private static string outputDir = @"d:\Temp\Wikipedia\";
+        private static string siteLink = "https://en.wikipedia.org";
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press any key for starting...");
+            Console.ReadKey();
+
+            RipperManager ripM = new RipperManager(siteLink, outputDir, DownloadOptionDomain.Domain, DownloadOptionLevel.Level1);
+
+            ripM.SaveSite();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module 1/RipperManager/Utils/UtilsLib.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RipperManagerLib
+{
+    public static class UtilsLib
+    {
+        public static string GetFileNameFromUrl(string url)
+        {
+            Uri uri;
+            if (!Uri.TryCreate(url, UriKind.Absolute, out uri))
+            {
+                Console.WriteLine("This reference (url) file name is not found");
+                return "";
+            }
+
+            return Path.GetFileName(uri.LocalPath);
+        }
+
+        public static void SaveFile(string fileName, string content)
+        {
+            try
+            {
+                Console.WriteLine("Save file {0}", fileName);
+
+                // создаем директорию для сохранения файла если ее нет
+                DirectoryInfo dirInfo = new DirectoryInfo(Path.GetDirectoryName(fileName));
+                if (!dirInfo.Exists)
+                    dirInfo.Create();
+
+                using (StreamWriter fileSave = new StreamWriter(fileName))
+                {
+                    fileSave.Write(content);
+                    fileSave.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void SaveFile(string fileName, Stream content)
+        {
+            try
+            {
+                Console.WriteLine("Save file {0}", fileName);
+
+                // создаем директорию для сохранения файла если ее нет
+                DirectoryInfo dirInfo = new DirectoryInfo(Path.GetDirectoryName(fileName));
+                if (!dirInfo.Exists)
+                    dirInfo.Create();
+
+                using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    content.CopyTo(fs);
+                    fs.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
